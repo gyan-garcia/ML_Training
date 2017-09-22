af86b7de925d803db99ea778b6de10d5f2ba9e16
Gyan Garcia Avila <ggarcia@microsoft.com>
2017-09-22 14:16:44 -0700

    Update to the Deep learning regression problem

--- ann_linear_classification.py
+++ ann_linear_classification.py
@@ -11,73 +11,109 @@ from keras.layers import Dense
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_classification_model_decision_boundary(model, features, labels):
+    xx, yy = np.mgrid[0:850:5, 0:850:5]
+    
+    grid = np.c_[xx.ravel(), yy.ravel()]
+    predicted_grid_probabilities = model.predict_proba(grid)
+    
+    f, ax = plt.subplots(figsize=(8, 6))
+    #http://matplotlib.org/examples/color/colormaps_reference.html
+    contour = ax.contourf(xx, yy, predicted_grid_probabilities.reshape(xx.shape[0],yy.shape[0]), 25, cmap="coolwarm",
+                          vmin=0, vmax=1)
+    ax_c = f.colorbar(contour)
+    
+    x_coords = features[0:features.shape[0], 0:1]
+    y_coords = features[0:features.shape[0], 1:2]
+    
+    ax.scatter(x_coords, y_coords, c=labels, s=50, cmap="coolwarm", vmin=-.2, vmax=1.2, edgecolor="white", linewidth=1)
+    plt.show()
+
+
+
+def plot_dataset(features, labels):
+    x_coords = features[0:features.shape[0], 0:1]
+    y_coords = features[0:features.shape[0], 1:2]
+    
+    f, ax = plt.subplots(figsize=(8, 6))    
+    ax.scatter(x_coords, y_coords, c=labels, s=50, cmap="coolwarm", vmin=-.2, vmax=1.2, edgecolor="white", linewidth=1)   
+    plt.show()
+
+
+
+def train_model(features, labels, hidden_activation, output_activation, loss_function):   
+    # create model
+    model = Sequential()
+    model.add(Dense(12, input_dim=2, activation=hidden_activation)) 
+    model.add(Dense(8, activation=hidden_activation))
+    model.add(Dense(1, activation=output_activation))
+    
+    # Compile model
+    model.compile(loss=loss_function, optimizer='adam', metrics=['accuracy']) #loss='binary_crossentropy' # optimizer='adam'
+    # loss function should be mse for linear
+    # and binary_crossentropy for sigmoid (binary classification problem)
+        
+    # Fit the model
+    model.fit(features, labels, epochs=300, batch_size=10)
+    return model
+    
+
 
 # fix random seed for reproducibility
 np.random.seed(7)
-
-
 os.chdir('C:\git\ML_Training') 
 
 
-# load dataframe
+
+# load linear dataset
 df = pd.read_csv('.\\regression_data.csv')
+features = np.array(df.as_matrix(columns= df.columns[0:2]))
+labels = np.array(df.as_matrix(columns= df.columns[2:3]))
+    
+# Let's take a look into our dataset
+plot_dataset(features, labels)
 
-X = np.array(df.as_matrix(columns= df.columns[0:2]))
-Y = np.array(df.as_matrix(columns= df.columns[2:3]))
-
-
-# create model
-model = Sequential()
-#model.add(Dense(12, input_dim=2, activation='relu', kernel_initializer='uniform'))
-model.add(Dense(4, input_dim=2, activation='linear')) #relu
-#model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='linear')) #sigmoid
-
-
-# Compile model
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy']) #loss='binary_crossentropy' # optimizer='adam'
-
-# Fit the model
-model.fit(X, Y, epochs=500, batch_size=10)
-
-# Normaly when the activation is linear, the loss should be MSE
-# When the activation is sigmoid, the loss should be binary_crossentropy for a binary classification problem
+# Train the classifier and plot the classification decision boundary
+model = train_model(features, labels, hidden_activation='linear', output_activation='linear', loss_function='mse')
+plot_classification_model_decision_boundary(model, features, labels)
 
 
 
+# load non-linear dataset
+df = pd.read_csv('.\\regression_data_2.csv')
+features = np.array(df.as_matrix(columns= df.columns[0:2]))
+labels = np.array(df.as_matrix(columns= df.columns[2:3]))
+    
+# Let's take a look into our dataset
+plot_dataset(features, labels)
+
+# Let's train a linear classifier to see how it behaves, 
+model = train_model(features, labels, hidden_activation='linear', output_activation='linear', loss_function='mse')
+plot_classification_model_decision_boundary(model, features, labels)
+
+# Let's trains a non linear classifier.
+model = train_model(features, labels, hidden_activation='relu', output_activation='sigmoid', loss_function='binary_crossentropy')
+plot_classification_model_decision_boundary(model, features, labels)
 
 
+# For more information of what loss funtions and optimizers to use: https://keras.io/getting-started/sequential-model-guide/#compilation
 
+# For a multi-class classification problem
+#model.compile(optimizer='rmsprop',
+#              loss='categorical_crossentropy',
+#              metrics=['accuracy'])
 
+# For a binary classification problem
+#model.compile(optimizer='rmsprop',
+#              loss='binary_crossentropy',
+#              metrics=['accuracy'])
 
-xx, yy = np.mgrid[0:850:5, 0:850:5]
-
-grid = np.c_[xx.ravel(), yy.ravel()]
-predicted_grid_probabilities = model.predict_proba(grid)
-
-f, ax = plt.subplots(figsize=(8, 6))
-#http://matplotlib.org/examples/color/colormaps_reference.html
-contour = ax.contourf(xx, yy, predicted_grid_probabilities.reshape(xx.shape[0],xx.shape[0]), 25, cmap="coolwarm",
-                      vmin=0, vmax=1)
-ax_c = f.colorbar(contour)
-
-x_coords = X[0:X.shape[0], 0:1]
-y_coords = X[0:X.shape[0], 1:2]
-
-ax.scatter(x_coords, y_coords, c=Y, s=50,
-           cmap="coolwarm", vmin=-.2, vmax=1.2,
-           edgecolor="white", linewidth=1)
-
-
-
-
-
-
-
-
-
-
-
+# For a mean squared error regression problem
+#model.compile(optimizer='rmsprop',
+#              loss='mse')
 
 
 
