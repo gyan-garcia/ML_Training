d12ebb3e99d76d1e69935fc1068a055e02436b8d
Gyan Garcia Avila <ggarcia@microsoft.com>
2017-09-28 11:39:23 -0700

    Updating the linear classification problem

--- ann_linear_classification.py
+++ ann_linear_classification.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 
 def plot_classification_model_decision_boundary(model, features, labels):
@@ -44,7 +45,7 @@ def plot_dataset(features, labels):
 
 
 
-def train_model(features, labels, hidden_activation, output_activation, loss_function):   
+def train_model(features, labels, hidden_activation, output_activation, loss_function, iterations):   
     # create model
     model = Sequential()
     model.add(Dense(12, input_dim=2, activation=hidden_activation)) 
@@ -57,28 +58,52 @@ def train_model(features, labels, hidden_activation, output_activation, loss_fun
     # and binary_crossentropy for sigmoid (binary classification problem)
         
     # Fit the model
-    model.fit(features, labels, epochs=300, batch_size=10)
+    model.fit(features, labels, epochs=iterations, batch_size=10)
     return model
     
 
 
 # fix random seed for reproducibility
 np.random.seed(7)
+
+# Set this to wherever you have downloaded the training data.
 os.chdir('C:\git\ML_Training') 
 
 
-
 # load linear dataset
-df = pd.read_csv('.\\regression_data.csv')
+df = pd.read_csv('.\\regression_linear_data.csv')
 features = np.array(df.as_matrix(columns= df.columns[0:2]))
 labels = np.array(df.as_matrix(columns= df.columns[2:3]))
-    
+   
 # Let's take a look into our dataset
 plot_dataset(features, labels)
 
+# Let's separate our data into a training and a test set
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.2)
+
 # Train the classifier and plot the classification decision boundary
-model = train_model(features, labels, hidden_activation='linear', output_activation='linear', loss_function='mse')
+model = train_model(features_train, labels_train, hidden_activation='linear', output_activation='linear', loss_function='mse', iterations=300)
 plot_classification_model_decision_boundary(model, features, labels)
+
+
+# Now score the model with data that it has not seen before.
+scores = model.evaluate(features_test, labels_test, verbose=0)
+print('test score:', scores[0])
+print('test accuracy:', scores[1])
+
+
+
+
+# Great, can we make then the output to be a probability? Yes, the only thing we need to to use 'sigmoid' as the activation method of the outout neuron.
+# Also, we need to use 'binary_crossentropy' as the loss function for sigmoid.
+model = train_model(features_train, labels_train, hidden_activation='linear', output_activation='sigmoid', loss_function='binary_crossentropy', iterations=500)
+plot_classification_model_decision_boundary(model, features, labels)
+
+# Now score the model with data that it has not seen before.
+scores = model.evaluate(features_test, labels_test, verbose=0)
+print('test score:', scores[0])
+print('test accuracy:', scores[1])
+
 
 
 
@@ -90,13 +115,30 @@ labels = np.array(df.as_matrix(columns= df.columns[2:3]))
 # Let's take a look into our dataset
 plot_dataset(features, labels)
 
+#Let's divide our train and test set
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.2)
+
+
 # Let's train a linear classifier to see how it behaves, 
-model = train_model(features, labels, hidden_activation='linear', output_activation='linear', loss_function='mse')
+model = train_model(features_train, labels_train, hidden_activation='linear', output_activation='linear', loss_function='mse', iterations=300)
 plot_classification_model_decision_boundary(model, features, labels)
 
-# Let's trains a non linear classifier.
-model = train_model(features, labels, hidden_activation='relu', output_activation='sigmoid', loss_function='binary_crossentropy')
+# Now score the model with data that it has not seen before.
+scores = model.evaluate(features_test, labels_test, verbose=0)
+print('test score:', scores[0])
+print('test accuracy:', scores[1])
+
+
+
+
+# Let's train a non linear classifier.
+model = train_model(features, labels, hidden_activation='relu', output_activation='sigmoid', loss_function='binary_crossentropy', iterations=500)
 plot_classification_model_decision_boundary(model, features, labels)
+
+# Now score the model with data that it has not seen before.
+scores = model.evaluate(features_test, labels_test, verbose=0)
+print('test score:', scores[0])
+print('test accuracy:', scores[1])
 
 
 # For more information of what loss funtions and optimizers to use: https://keras.io/getting-started/sequential-model-guide/#compilation
